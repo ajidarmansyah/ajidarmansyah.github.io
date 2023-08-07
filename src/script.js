39f8101bc8ae2256e47833c99afaddea2f79e5a6
aji darmansyah <ajidarmansyah461@gmail.com>
2023-08-07 09:06:12 +0700

    menambahkan fitur api whatsapp ketika tombol diclick

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
 const nav = document.getElementById('navigasi');
 const navButton = document.getElementById('navbutton');
+const btnHubungiSaya = document.getElementById('btn-hubungi-saya');
 
 navButton.addEventListener('click', function () {
     nav.classList.toggle('hidden');
-})
+});
+
+const btnHandelerHubungiSaya = () => {
+    const phoneNumber = '081383614967'
+    const message = 'Halo saya tertarik dengan portfolio anda!';
+    
+    const url = 'https://api.whatsapp.com/send?phone=' + phoneNumber + '&text=' + encodeURIComponent(message);
+
+    window.open(url, '_blank');
+};
+
+btnHubungiSaya.addEventListener('click', btnHandelerHubungiSaya);
